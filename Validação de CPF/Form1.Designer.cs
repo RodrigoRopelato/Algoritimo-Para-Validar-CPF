0b675540cb87fc09a739b25ff6c9ad7ca8086788
Rodrigo Ropelato <rodrigoropelato@hotmail.com>
2021-10-31 13:53:30 -0300

    versão entregue no trabalho senai

---
+++ Validação de CPF/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Validação_de_CPF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ttbCPF = new System.Windows.Forms.MaskedTextBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.lbSituacao = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(86, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VALIDAR CPF";
+            // 
+            // ttbCPF
+            // 
+            this.ttbCPF.CutCopyMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            this.ttbCPF.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ttbCPF.Location = new System.Drawing.Point(86, 56);
+            this.ttbCPF.Mask = "000,000,000-00";
+            this.ttbCPF.Name = "ttbCPF";
+            this.ttbCPF.Size = new System.Drawing.Size(152, 33);
+            this.ttbCPF.TabIndex = 1;
+            this.ttbCPF.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnValidar.Location = new System.Drawing.Point(86, 95);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(152, 33);
+            this.btnValidar.TabIndex = 2;
+            this.btnValidar.Text = "VALIDAR";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // lbSituacao
+            // 
+            this.lbSituacao.AutoSize = true;
+            this.lbSituacao.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbSituacao.Location = new System.Drawing.Point(91, 164);
+            this.lbSituacao.Name = "lbSituacao";
+            this.lbSituacao.Size = new System.Drawing.Size(0, 37);
+            this.lbSituacao.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(13, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Situação:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 223);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbSituacao);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.ttbCPF);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.Text = "CPF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox ttbCPF;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Label lbSituacao;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Validação de CPF/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Validação_de_CPF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbSituacao.Text = "";
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+                string stringCPF = ttbCPF.Text;
+                if(stringCPF.Length == 11)
+                    ValidarCPF(stringCPF);
+                else
+                    MessageBox.Show("O Campo do CPF esta incompleto!", "Mensagem do Sistema");
+        }
+
+        private void ValidarCPF(string cpf)
+        {
+            //para criar esse algoritimo foi utilizado como base o algoritimo do site: http://www.macoratti.net/alg_cpf.htm
+            int resultado1 = 0;
+            int resultado2 = 0;
+            int dig1 = 0;
+            int dig2 = 0;
+            int[] vetorCpf = new int[11];
+            for (int i = 0; i < vetorCpf.Length; i++)
+                vetorCpf[i] = int.Parse(cpf[i].ToString());
+
+            //primeira verificação, verifica os 9 primeiros digitos do CPF e faz o calculo para chegar no primeriro digito verificadr
+            int num = 2;
+            for (int i = 8; i >=0; i--)
+            {
+                resultado1 += vetorCpf[i] * num;
+                num++;
+            }
+            //calcula e atribui o resultado do primeiro digito
+            if (resultado1 % 11 < 2)
+                dig1 = 0;
+            else dig1 = 11 - (resultado1 % 11);
+
+            //segunda verificação, inclui o primeiro digito verificador para chegar no segundo digito
+            num = 2;
+            for (int i = 9; i >= 0; i--)
+            {
+                resultado2 += vetorCpf[i] * num;
+                num++;
+            }
+            //calcula e atribui o resultado do segundo digito
+            if (resultado2 % 11 < 2)
+                dig2 = 0;
+            else dig2 = 11 - (resultado2 % 11);
+
+            //compara digitos obtidos do algorito com os digitos fornecidos pelo usuario
+            if (dig1 == vetorCpf[9] && dig2 == vetorCpf[10])
+                lbSituacao.Text = "CPF Valido!";
+            else
+                lbSituacao.Text = "CPF Invalido!";
+        }
+    }
+}
